54e4e672c0451a0194a14cbd5f8cb7eb992adf38
paddywej <paddy.wej@gmail.com>
2024-11-17 23:58:15 +0700

    program pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import About from "./pages/About/About"; 
 import HomePage from './pages/HomePage/HomePage';
 import Program from "./pages/Program/Program";
+import Glasgow from "./pages/Program/Glasgow";
+import Queensland from "./pages/Program/Queensland";
+import Exchange from "./pages/Program/Exchange";
+import Internships from "./pages/Program/Internships";
 import Event from "./pages/Event/Event";
 import News from "./pages/News/News";
 import Admission from "./pages/Admission/Admission";
@@ -36,7 +40,11 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage {...loginProps} />} />
       <Route path="/about" element={<About {...loginProps} />} />
-      <Route path="/program" element={<Program {...loginProps} />} />
+      <Route path="/program" element={<Program />} />
+      <Route path="/glasgow" element={<Glasgow />} />
+      <Route path="/queensland" element={<Queensland />} />
+      <Route path="/exchange" element={<Exchange />} />
+      <Route path="/internships" element={<Internships />} />
       <Route path="/event" element={<Event {...loginProps} />} />
       <Route path="/news" element={<News {...loginProps} />} />
       <Route path="/admission" element={<Admission {...loginProps} />} />
